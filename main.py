693661e610e19699d58e89a590d328dbaa6f166e
Adelina <115109011+adelhor@users.noreply.github.com>
2023-10-03 19:47:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-#import math
 from reverbTime import *
-import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = 'Y1012Jqkhkp',
-    database = 'coefficient'
-)
-
-mycursor=mydb.cursor()
-'''
-mycursor.execute("SELECT * FROM coefficient")
-result = mycursor.fetchall()
-print(result)
-'''
-
-#space_type=input('What is the type of the space which you want to calculate the reverberation time? \n')
 material = input('Write the material of the first surface \n')
-sql = "SELECT * FROM coefficient WHERE MATERIAL = %s"
-val = (material,)
-mycursor.execute(sql, val)
-my_material = mycursor.fetchall()
-print(my_material)
 
-out = []
-for x in my_material:
-    for item in x:
-        out.append(item)
+area = float(input('What is the size of surface? \n'))
 
-surface = float(input('What is the size of surface? \n'))
+type_of_space = input('Write the type of space \n')
 
-x=2
-while x < len(out):
-    a = round(out[x]*surface,2)
-    print(a)
-    x = x + 1
+volume = float(input('What is the volume of space? \n'))
+
+Space(type_of_space, volume, area, material)
