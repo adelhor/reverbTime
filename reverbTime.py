ea7f6ba880cfb5e87123676ec88c53b65543d5ec
Adelina <115109011+adelhor@users.noreply.github.com>
2023-10-05 19:36:24 +0200

    Update reverbTime.py

--- reverbTime.py
+++ reverbTime.py
@@ -1,5 +1,5 @@
-from abc import ABC
 import mysql.connector
+import matplotlib.pyplot as plt
 
 mydb = mysql.connector.connect(
     host = 'localhost',
@@ -10,27 +10,19 @@ mydb = mysql.connector.connect(
 
 mycursor=mydb.cursor()
 
-class Space(ABC):
-    '''
-    @property
-    def reverberation_time(self):
-        return self._reverberation_time
-    @reverberation_time.setter
-    def reverberation_time(self, value):
-        #value = round(value,1)
-        self._reverberation_time.append( value )
-    '''
-    def __init__(self,type_of_space,volume, area, material):
+class Space():
+    def __init__(self,type_of_space,volume):
         self.type_of_space = type_of_space
         self.volume = volume
-        self.area = area
-        self.material = material
-        #self.absorption()
+        #self.area = area
+        #self.material = material
+        self.absorption()
         self.calculation()
 
-    def absorption(self):
+    @classmethod
+    def absorption(cls, area, material):
         sql = "SELECT * FROM coefficient WHERE MATERIAL = %s"
-        val = (self.material,)
+        val = (material,)
         mycursor.execute(sql, val)
         my_material = mycursor.fetchall()
         out = []
@@ -42,14 +34,21 @@ class Space(ABC):
         global total_absorption
         total_absorption=[]
         while x < len(out):
-            a = round(out[x]*self.area,2)
+            a = round(out[x]*area,2)
             total_absorption.append(a)
             x = x + 1
-        print(total_absorption)
+        return total_absorption
+        #print(total_absorption)
 
-    def calculation(self):
-        self.absorption()
+    def calculation(volume, total_absorption):
         reverberation_time = []
         for i in total_absorption:
-            reverberation_time.append(round((0.161*self.volume)/i,1))
+            reverberation_time.append(round((0.161*volume)/i,1))
         print(reverberation_time)
+        x = [125, 250, 500, 1000, 2000, 4000]
+        plt.plot(x,reverberation_time)
+        plt.xscale("log", base=2)
+        #plt.xlim(125,4000)
+        plt.xticks([125, 250, 500, 1000, 2000, 4000])
+        plt.grid(True)
+        plt.show()
