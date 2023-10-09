d10861799470eee75b92b0b6c26a15005215bfed
Adelina <115109011+adelhor@users.noreply.github.com>
2023-10-09 15:53:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from reverbTime import *
-#import matplotlib.pyplot as plt
 import numpy as np
+from DB import *
 
-type_of_space = input('Write the type of space \n')
+type_of_space = input('Write the type of space that you wish to calculate the reverberation time. \n')
 
 volume = float(input('What is the volume of space? \n'))
 
 total_absorption = [0] * 6
 
 while True:
-    user_option = int(input("Press 1 if you want add surface to your space. Press 2 if you finish adding surfaces and want to calculate the reverberation time \n"))
+    user_option = int(input("Press 1 if you want add surface to your space. Press 2 if you finish adding surfaces and want to calculate the reverberation time \n" 
+                            "Press 3 if you want to add the metarial's absorption coefficient to database and press 4 if you want to see database of coefficients \n"))
     
     if user_option == 1:
 
@@ -23,5 +24,18 @@ while True:
         print('The reverberation time has been calculated')
         break
 
+    elif user_option == 3:
+        material_name = input('Write the name of the material that you want to add \n')
+        Hz125 = float(input('Write the absorption coefficent for 125 Hz \n'))
+        Hz250 = float(input('Write the absorption coefficent for 250 Hz \n'))
+        Hz500 = float(input('Write the absorption coefficent for 500 Hz \n'))
+        Hz1000 = float(input('Write the absorption coefficent for 1000 Hz \n'))
+        Hz2000 = float(input('Write the absorption coefficent for 2000 Hz \n'))
+        Hz4000 = float(input('Write the absorption coefficent for 4000 Hz \n'))
+        DB.write_material(material_name, Hz125, Hz250, Hz500, Hz1000, Hz2000, Hz4000)
+
+    elif user_option == 4:
+        DB.show_db()
+    
     else:
         break
